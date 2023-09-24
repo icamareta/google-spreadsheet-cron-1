141e92690799dca7b1ff83d6d9138be614c92bea
indipound <altongunawan@icloud.com>
2023-09-24 23:03:44 +0800

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,14 +3,17 @@ import { useSearchParams } from "next/navigation";
 import { getValues } from "@/actions/function-xyz";
 
 export async function GET(request: NextRequest) {
-    return NextResponse.json({value: ""});
-//   const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
 
-//   const minVal = searchParams.get("min-val") as string;
-//   const maxVal = searchParams.get("max-val") as string;
+  const minVal = searchParams.get("min-val") as string;
+  const maxVal = searchParams.get("max-val") as string;
 
-//   console.log(minVal)
-//   console.log(maxVal)
+  console.log(minVal)
+  console.log(maxVal)
+  
+  return NextResponse.json({value: ""});
+
+
 
 //   return await getValues(+minVal, +maxVal)
 //     .then((value) => {
