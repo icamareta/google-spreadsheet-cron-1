87b2ac773337a195f7f9ec9d2ca45bf98e00ea48
indipound <altongunawan@icloud.com>
2023-09-24 23:02:40 +0800

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,21 +3,22 @@ import { useSearchParams } from "next/navigation";
 import { getValues } from "@/actions/function-xyz";
 
 export async function GET(request: NextRequest) {
-  const searchParams = useSearchParams();
+    return NextResponse.json({value: ""});
+//   const searchParams = useSearchParams();
 
-  const minVal = searchParams.get("min-val") as string;
-  const maxVal = searchParams.get("max-val") as string;
+//   const minVal = searchParams.get("min-val") as string;
+//   const maxVal = searchParams.get("max-val") as string;
 
-  console.log(minVal)
-  console.log(maxVal)
+//   console.log(minVal)
+//   console.log(maxVal)
 
-  return await getValues(+minVal, +maxVal)
-    .then((value) => {
-      console.log("Success get values");
-      console.log(value);
-      return NextResponse.json(value);
-    })
-    .catch((error) => {
-      return NextResponse.json(error);
-    });
+//   return await getValues(+minVal, +maxVal)
+//     .then((value) => {
+//       console.log("Success get values");
+//       console.log(value);
+//       return NextResponse.json(value);
+//     })
+//     .catch((error) => {
+//       return NextResponse.json(error);
+//     });
 }
