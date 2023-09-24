e33f754db3e6602b2925612841e6541e9dc503a0
indipound <altongunawan@icloud.com>
2023-09-24 23:08:17 +0800

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,27 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
-import { useSearchParams } from "next/navigation";
 import { getValues } from "@/actions/function-xyz";
 
 export async function GET(request: NextRequest) {
-  const searchParams = useSearchParams();
+  const url = new URL(request.url);
+  const searchParams = new URLSearchParams(url.search);
 
-  const minVal = searchParams.get("min-val") as string || "";
-  const maxVal = searchParams.get("max-val") as string || "";
+  const minVal = (searchParams.get("min-val") as string) || "";
+  const maxVal = (searchParams.get("max-val") as string) || "";
 
-  console.log(minVal)
-  console.log(maxVal)
+  console.log(minVal);
+  console.log(maxVal);
 
-  return NextResponse.json({value: ""});
+  return NextResponse.json({ value: "" });
 
-
-
-//   return await getValues(+minVal, +maxVal)
-//     .then((value) => {
-//       console.log("Success get values");
-//       console.log(value);
-//       return NextResponse.json(value);
-//     })
-//     .catch((error) => {
-//       return NextResponse.json(error);
-//     });
+  //   return await getValues(+minVal, +maxVal)
+  //     .then((value) => {
+  //       console.log("Success get values");
+  //       console.log(value);
+  //       return NextResponse.json(value);
+  //     })
+  //     .catch((error) => {
+  //       return NextResponse.json(error);
+  //     });
 }
